b4dbd70a159fb1c01aedf63e265f73f5d8e10c5e
Dion Garman <diongarman@gmail.com>
2020-04-13 11:48:19 +0100

    improved readability

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,15 @@ const matches = fs
 
 console.log(matches);
 
+const HomeWin = 'H';
+const AwayWin = 'A';
+
 let manUnitedWins = 0;
 
 for (let match of matches) {
   if (
-    (match[1] === 'Man United' && match[5] === 'H') ||
-    (match[2] === 'Man United' && match[5] === 'A')
+    (match[1] === 'Man United' && match[5] === HomeWin) ||
+    (match[2] === 'Man United' && match[5] === AwayWin)
   ) {
     manUnitedWins++;
   }
